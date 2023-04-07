dfdd6d827a929e1482d04c4939681dba2ebcf6c7
602311329 <93909893+602311329@users.noreply.github.com>
2023-04-07 15:26:51 +0800

    完善2

--- blog-api-my/src/main/java/com/txgc/blog/dao/mapper/ArticleTagMapper.java
+++ blog-api-my/src/main/java/com/txgc/blog/dao/mapper/ArticleTagMapper.java
@@ -2,6 +2,8 @@ package com.txgc.blog.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.txgc.blog.dao.pojo.ArticleTag;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleTagMapper extends BaseMapper<ArticleTag> {
 }
